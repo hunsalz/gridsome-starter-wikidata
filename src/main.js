139693e3e0b5a7418863b18eb584c404d429cd47
Markus Hunsalz <markushunsalz@Markuss-MBP.fritz.box>
2019-12-21 22:30:59 +0100

    added comment

--- src/main.js
+++ src/main.js
@@ -7,9 +7,9 @@ import VueLodash from "vue-lodash";
 export default function(Vue, { router, head, isClient }) {
   // set default layout as a global component
   Vue.component("Layout", DefaultLayout);
-
-  Vue.prototype.$eventBus = new Vue(); // global event bus
-
+  // set global event bus
+  Vue.prototype.$eventBus = new Vue(); 
+  // set global lodash
   const options = { name: "lodash" };
   Vue.use(VueLodash, options);
 }
