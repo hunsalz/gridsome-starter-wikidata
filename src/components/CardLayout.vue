8976fcf09bd4b3b48fe28252123bf996a186f0df
Markus Hunsalz <markushunsalz@Markuss-MBP.fritz.box>
2020-01-06 01:19:27 +0100

    --wip--

--- src/components/CardLayout.vue
+++ src/components/CardLayout.vue
@@ -1,5 +1,31 @@
 <template>
   <div class="card-layout content-box">
+    <!--div class="card-layout__favorite">LIKE</div-->
+
+    <button
+      role="button"
+      aria-label="Toggle favorite"
+      class="card-layout__favorite"
+      @click.prevent="toggleTheme"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-sun"
+      >
+        <path
+          d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"
+        />
+      </svg>
+    </button>
+
     <div class="card-layout__header">
       <g-image
         v-if="record.cover_image"
@@ -50,6 +76,21 @@ export default {
 .card-layout {
   position: relative;
 
+  &__favorite {
+    position: absolute;
+    right: 0.5em;
+    top: 0.5em;
+    z-index: 1;
+    margin: 0.2em;
+    padding: 0.4em;
+    background-color: var(--bg-color);
+    color: currentColor !important;
+    border: 0;
+    border-radius: var(--radius);
+    white-space: nowrap;
+    opacity: 0;
+  }
+
   &__header {
     border-radius: var(--radius) var(--radius) 0 0;
     overflow: hidden;
@@ -73,14 +114,14 @@ export default {
   }
 
   &__tags {
-    z-index: 1;
     position: relative;
+    z-index: 1;
   }
 
   &__link {
-    color: currentColor !important;
-    z-index: 1;
     position: relative;
+    z-index: 1;
+    color: currentColor !important;
   }
 
   &__link_to_record {
@@ -91,6 +132,16 @@ export default {
     height: 100%;
     opacity: 0;
     z-index: 0;
+  }
+}
+
+.card-layout:hover {
+  .card-layout__favorite {
+    // &__favorite {} doesn't work
+    position: absolute;
+    right: 0.5em;
+    top: 0.5em;
+    opacity: 1;
   }
 }
 </style>
