183c93a38678b8854c6e93130dd15aea4ffe5819
Markus Hunsalz <markushunsalz@Markuss-MBP.fritz.box>
2020-03-21 19:43:59 +0100

    use action-button

--- src/components/TagCloud.vue
+++ src/components/TagCloud.vue
@@ -5,7 +5,7 @@
       :key="index"
       role="button"
       aria-label="Tag"
-      class="tag-filter__button"
+      class="action-button"
       @click.prevent="emitEvent(tag)"
     >
       <span># {{ tag }}</span>
@@ -36,24 +36,6 @@ export default {
 <style lang="scss">
 .tag-filter {
   margin: 1em 0 1em 0;
-
-  &__button {
-    margin: 0.2em;
-    padding: 0.5em;
-    font-size: 0.8em;
-    text-decoration: none;
-    background-color: var(--bg-color);
-    color: currentColor;
-    fill: currentColor;
-    border-radius: var(--radius);
-    border-style: solid;
-    border-width: 1px;
-    border-color: currentColor;
-    box-shadow: 1px 1px 5px 0 rgba(0, 0, 0, 0.1);
-    opacity: 0.6;
-    white-space: nowrap;
-    cursor: pointer;
-  }
 
   :hover {
     opacity: 1;
